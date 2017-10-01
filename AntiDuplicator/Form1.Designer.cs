07c09137ccd1582427d5407d0483454d9e7090c2
Komron Mukhamadulloev <disqrl@yandex.ru>
2017-10-01 22:06:12 +0500

    Initial upload

---
+++ AntiDuplicator/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace AntiDuplicator
+{
+    partial class ad
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.filesGrid = new System.Windows.Forms.DataGridView();
+            this.scanFileName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.scanFileSize = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.scanFileHash = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.scanFileDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.scanFilePath = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.scanPath = new System.Windows.Forms.TextBox();
+            this.scanAction = new System.Windows.Forms.Button();
+            this.actionGo = new System.Windows.Forms.Button();
+            this.adv = new System.Windows.Forms.PictureBox();
+            this.addScanPath = new System.Windows.Forms.FolderBrowserDialog();
+            this.action = new System.Windows.Forms.ComboBox();
+            this.scanProcess = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.filesGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.adv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // filesGrid
+            // 
+            this.filesGrid.AllowUserToAddRows = false;
+            this.filesGrid.AllowUserToDeleteRows = false;
+            this.filesGrid.AllowUserToResizeRows = false;
+            this.filesGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.filesGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.filesGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.scanFileName,
+            this.scanFileSize,
+            this.scanFileHash,
+            this.scanFileDate,
+            this.scanFilePath});
+            this.filesGrid.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.filesGrid.Location = new System.Drawing.Point(12, 35);
+            this.filesGrid.Name = "filesGrid";
+            this.filesGrid.ShowEditingIcon = false;
+            this.filesGrid.Size = new System.Drawing.Size(680, 325);
+            this.filesGrid.TabIndex = 0;
+            // 
+            // scanFileName
+            // 
+            this.scanFileName.HeaderText = "FileName";
+            this.scanFileName.Name = "scanFileName";
+            this.scanFileName.Width = 150;
+            // 
+            // scanFileSize
+            // 
+            this.scanFileSize.HeaderText = "FileSize";
+            this.scanFileSize.Name = "scanFileSize";
+            // 
+            // scanFileHash
+            // 
+            this.scanFileHash.HeaderText = "FileHash";
+            this.scanFileHash.Name = "scanFileHash";
+            this.scanFileHash.Width = 200;
+            // 
+            // scanFileDate
+            // 
+            this.scanFileDate.HeaderText = "FileDate";
+            this.scanFileDate.Name = "scanFileDate";
+            // 
+            // scanFilePath
+            // 
+            this.scanFilePath.HeaderText = "FilePath";
+            this.scanFilePath.Name = "scanFilePath";
+            this.scanFilePath.Width = 250;
+            // 
+            // scanPath
+            // 
+            this.scanPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.scanPath.Location = new System.Drawing.Point(12, 8);
+            this.scanPath.Name = "scanPath";
+            this.scanPath.ReadOnly = true;
+            this.scanPath.Size = new System.Drawing.Size(300, 20);
+            this.scanPath.TabIndex = 1;
+            this.scanPath.Click += new System.EventHandler(this.textBox1_Click);
+            // 
+            // scanAction
+            // 
+            this.scanAction.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.scanAction.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.scanAction.Location = new System.Drawing.Point(318, 5);
+            this.scanAction.Name = "scanAction";
+            this.scanAction.Size = new System.Drawing.Size(75, 24);
+            this.scanAction.TabIndex = 2;
+            this.scanAction.Text = "Scan";
+            this.scanAction.UseVisualStyleBackColor = true;
+            this.scanAction.Click += new System.EventHandler(this.scanAction_Click);
+            // 
+            // actionGo
+            // 
+            this.actionGo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.actionGo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.actionGo.Location = new System.Drawing.Point(617, 5);
+            this.actionGo.Name = "actionGo";
+            this.actionGo.Size = new System.Drawing.Size(75, 24);
+            this.actionGo.TabIndex = 3;
+            this.actionGo.Text = "GO!";
+            this.actionGo.UseVisualStyleBackColor = true;
+            // 
+            // adv
+            // 
+            this.adv.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.adv.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.adv.Location = new System.Drawing.Point(12, 366);
+            this.adv.Name = "adv";
+            this.adv.Size = new System.Drawing.Size(680, 64);
+            this.adv.TabIndex = 5;
+            this.adv.TabStop = false;
+            // 
+            // action
+            // 
+            this.action.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.action.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.action.FormattingEnabled = true;
+            this.action.Items.AddRange(new object[] {
+            "DAR",
+            "DAO",
+            "DAY"});
+            this.action.Location = new System.Drawing.Point(399, 7);
+            this.action.MaxDropDownItems = 4;
+            this.action.Name = "action";
+            this.action.Size = new System.Drawing.Size(212, 21);
+            this.action.TabIndex = 6;
+            // 
+            // scanProcess
+            // 
+            this.scanProcess.WorkerReportsProgress = true;
+            this.scanProcess.DoWork += new System.ComponentModel.DoWorkEventHandler(this.scanProcess_DoWork);
+            this.scanProcess.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.scanProcess_ProgressChanged);
+            this.scanProcess.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.scanProcess_RunWorkerCompleted);
+            // 
+            // ad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 442);
+            this.Controls.Add(this.action);
+            this.Controls.Add(this.adv);
+            this.Controls.Add(this.actionGo);
+            this.Controls.Add(this.scanAction);
+            this.Controls.Add(this.scanPath);
+            this.Controls.Add(this.filesGrid);
+            this.Name = "ad";
+            this.Text = "AntiDuplicator";
+            ((System.ComponentModel.ISupportInitialize)(this.filesGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.adv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView filesGrid;
+        private System.Windows.Forms.TextBox scanPath;
+        private System.Windows.Forms.Button scanAction;
+        private System.Windows.Forms.Button actionGo;
+        private System.Windows.Forms.PictureBox adv;
+        private System.Windows.Forms.FolderBrowserDialog addScanPath;
+        private System.Windows.Forms.ComboBox action;
+        private System.ComponentModel.BackgroundWorker scanProcess;
+        private System.Windows.Forms.DataGridViewTextBoxColumn scanFileName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn scanFileSize;
+        private System.Windows.Forms.DataGridViewTextBoxColumn scanFileHash;
+        private System.Windows.Forms.DataGridViewTextBoxColumn scanFileDate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn scanFilePath;
+
+    }
+}
+
---
+++ AntiDuplicator/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace AntiDuplicator
+{
+    public partial class ad : Form
+    {
+        public string appName = "AntiDuplicator";
+
+        private class FileEntry
+        {
+            public string name;
+            public string size;
+            public string hash;
+            public string date;
+            public string path;
+            public string color;
+
+            public FileEntry(string name, string size, string hash, string date, string path)
+            {
+                this.name = name;
+                this.size = size;
+                this.hash = hash;
+                this.date = date;
+                this.path = path;
+                this.color = "white";
+            }
+
+            public bool isEqualName(FileEntry fe)
+            {
+                return this.name == fe.name;
+            }
+
+            public bool isEqualHash(FileEntry fe)
+            {
+                return this.hash == fe.hash;
+            }
+
+            public string compareWith(FileEntry fe)
+            {
+                if (this.isEqualName(fe) && this.isEqualHash(fe))
+                {
+                    this.color = "red";
+                    return this.color;
+                }
+
+                if (this.isEqualName(fe))
+                {
+                    this.color = "yellow";
+                    return this.color;
+                }
+
+                if (this.isEqualHash(fe))
+                {
+                    this.color = "orange";
+                    return this.color;
+                }
+
+                return null;
+            }
+        }
+
+        private class ScanProgress
+        {
+            public int current;
+            public int total;
+            public string status;
+
+            public ScanProgress(int current, int total, string status)
+            {
+                this.current = current;
+                this.total = total;
+                this.status = status;
+            }
+
+            public ScanProgress(int current, int total)
+            {
+                this.current = current;
+                this.total = total;
+                this.status = "";
+            }
+        }
+
+        public ad()
+        {
+            InitializeComponent();
+
+            this.Text = appName;
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            addScanPath.ShowDialog();
+            scanPath.Text = addScanPath.SelectedPath.ToString();
+        }
+
+        private void scanAction_Click(object sender, EventArgs e)
+        {
+            if (scanPath.Text == "")
+            {
+                MessageBox.Show("There is no path, please set path");
+            }
+            else
+            {
+                scanProcess.RunWorkerAsync();
+            }
+            /*List<List<string>> number = new List<List<string>>();
+            for (int j = 0; j < 10; j++)
+            {
+                number.Add(new List<string>());
+                number[j].Add("low: 10");
+                number[j].Add("high: 20");
+                MessageBox.Show(number[j][1].ToString());
+            }*/
+        }
+
+        private void scanProcess_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker bw = (BackgroundWorker)sender;
+            string[] scannedFilePath = Directory.GetFiles(scanPath.Text, "*.*", SearchOption.AllDirectories);
+            var entries = new List<FileEntry>();
+
+            for (int i = 0; i <= scannedFilePath.Length - 1; i++ )
+            {
+                string scannedFile = scannedFilePath[i];
+                string scannedFileName = Path.GetFileName(scannedFile);
+                FileInfo scannedFileInfo = new FileInfo(scannedFile);
+                int scannedFileSize = Convert.ToInt32(scannedFileInfo.Length / 1024);
+                string scannedFileMD5Hash = "";
+                byte[] inputBytes;
+                using (var md5 = MD5.Create())
+                {
+                    using (var stream = File.OpenRead(scannedFile))
+                    {
+                        inputBytes = md5.ComputeHash(stream);
+                        StringBuilder strMd5 = new StringBuilder();
+                        for (int j = 0; j < inputBytes.Length; j++)
+                        {
+                            strMd5.Append(inputBytes[j].ToString("x2"));
+                        }
+
+                        scannedFileMD5Hash = strMd5.ToString();
+                    }
+                }
+                string scannedFileDate = File.GetCreationTime(scannedFile).ToString();
+
+                FileEntry fData = new FileEntry(scannedFileName, scannedFileSize.ToString(), scannedFileMD5Hash, scannedFileDate, scannedFilePath[i]);
+                foreach (var en in entries)
+                {
+                    if (fData.compareWith(en) != null) break;
+                };
+
+                entries.Add(fData);
+
+                int length = scannedFilePath.Length - 1;
+                var progress = new ScanProgress(i, length);
+                bw.ReportProgress((i * 100) / length, progress);
+            }
+
+            // MessageBox.Show(outputData.Count.ToString());
+            
+            // var scanResult = outputData.ToArray();
+            // e.Result = scanResult;
+            e.Result = entries;
+        }
+
+        private void scanProcess_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            ScanProgress state = (ScanProgress)e.UserState;
+
+            this.Text = String.Format(
+                "{0} | Progress: {1} / {2} ({3}%)",
+                appName,
+                state.current,
+                state.total,
+                e.ProgressPercentage
+            );
+        }
+
+        private void scanProcess_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            ad.ActiveForm.Text = appName;
+
+            // var outData = (List<string>[])e.Result;
+            var outData = (List<FileEntry>)e.Result;
+
+            for (int i = 0; i < outData.Count; i++)
+            {
+                // filesGrid.Rows.Add(outData[i][0], outData[i][1], outData[i][2], outData[i][3], outData[i][4]);
+                filesGrid.Rows.Add(outData[i].name, outData[i].size, outData[i].hash, outData[i].date, outData[i].path);
+                for (int cellNum = 0; cellNum < filesGrid.Rows[i].Cells.Count; cellNum++)
+                {
+                    filesGrid.Rows[i].Cells[cellNum].Style.BackColor = Color.FromName(outData[i].color);
+                }
+            }
+        }
+    }
+}
